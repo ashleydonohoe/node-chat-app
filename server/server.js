5690528b163e364df01843157173d6dc4d55977d
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-05-02 23:57:30 -0400

    added greetings

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "admin",
+    text: "Welcome to the chat app!"
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "admin",
+    text: "New user joined"
+  });
+
   socket.on("createMessage", (newMessage) => {
     console.log("createMessage", newMessage);
     io.emit("newMessage", {
@@ -21,6 +31,11 @@ io.on("connection", (socket) => {
       text: newMessage.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => {
